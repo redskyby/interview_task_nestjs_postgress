7fc3faeabdef39a956a02f8d8f2e82d5d6367eb2
Docenko Pasha <pashadocenko@gmail.com>
2024-01-20 22:04:26 +0300

    Project was checked. (well) necessary dependencies were added (postgres and sequelize)

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import {config} from 'dotenv';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+
+config();
+
+const PORT = process.env.PORT || 7000;
+
+async function start(){
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(PORT , () => console.log(`Server was started on port = ${PORT}`));
+
+}
+
+
+start();
