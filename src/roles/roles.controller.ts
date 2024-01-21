d2557798c8e2647b128c5bd91ebd5a16bcfba175
Docenko Pasha <pashadocenko@gmail.com>
2024-01-21 18:33:17 +0300

    roles.controller.ts was done.

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { RolesService } from "./roles.service";
+import { Create_roleDto } from "./dto/create_role.dto";
 
 @Controller("roles")
 export class RolesController {
     constructor(private readonly rolesService: RolesService) {}
+    @Post()
+    create(@Body() dto: Create_roleDto) {
+        return this.rolesService.createRole(dto);
+    }
+    @Get("/:value")
+    getByValue(@Param("value") value: string) {
+        return this.rolesService.getRoleByValue(value);
+    }
 }
