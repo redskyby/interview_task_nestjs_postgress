a55327a0494f0498708ecc1e901ad380868898cc
Docenko Pasha <pashadocenko@gmail.com>
2024-01-21 21:12:40 +0300

    Auth module was added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { User } from "../users/users.model";
 import { RolesModule } from "../roles/roles.module";
 import { Role } from "../roles/roles.model";
 import { UserRoles } from "../userRoles/user_roles.model";
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
     controllers: [],
@@ -27,6 +28,7 @@ import { UserRoles } from "../userRoles/user_roles.model";
         }),
         UsersModule,
         RolesModule,
+      AuthModule
     ],
 })
 export class AppModule {}
