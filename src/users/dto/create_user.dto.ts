c7773d1ce055bca533101e3ea8971041da019ce5
Docenko Pasha <pashadocenko@gmail.com>
2024-01-21 14:28:39 +0300

    DTO was added.

---
+++ src/users/dto/create_user.dto.ts
@@ -0,0 +1,4 @@
+export class Create_userDto{
+  readonly email : string;
+  readonly password : string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { User } from './users.model';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  constructor(@InjectModel(User) private userRepository :typeof User) {
+  }
+
+  async createUser(){
+
+  }
+
+  async getAllUsers(){
+
+  }
+}
