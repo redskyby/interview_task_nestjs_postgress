c6d5b196a72ac2b0109c4b4e689c8f1101ad77c4
Docenko Pasha <pashadocenko@gmail.com>
2024-01-21 18:27:14 +0300

    roles.service.ts was done;

---
+++ src/roles/dto/create_role.dto.ts
@@ -0,0 +1,4 @@
+export class Create_roleDto {
+    readonly value: string;
+    readonly description: string;
+}
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { Create_roleDto } from "./dto/create_role.dto";
+import { InjectModel } from "@nestjs/sequelize";
+import { Role } from "./roles.model";
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+    constructor(@InjectModel(Role) private roleRepository: typeof Role) {}
+
+    async createRole(dto: Create_roleDto) {
+        return await this.roleRepository.create(dto);
+    }
+
+    async getRoleByValue(value: string) {
+        return await this.roleRepository.findOne({ where: { value } });
+    }
+}
