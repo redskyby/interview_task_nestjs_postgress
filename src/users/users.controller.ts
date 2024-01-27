f20c2ae4c69df35ee5578e5a68b2317350da6d9c
Docenko Pasha <pashadocenko@gmail.com>
2024-01-27 23:38:46 +0300

    1)validation.exception.ts was added. 2)validation.exception.ts was added to user.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Post, Get, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, Get, UseGuards, UsePipes } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { Create_userDto } from "./dto/create_user.dto";
 import { RolesGuard } from "../auth/roles.guard";
 import { Roles } from "../auth/roles_auth.decorator";
 import { Add_roleDto } from "./dto/add_role.dto";
 import { Ban_userDto } from "./dto/ban_user.dto";
+import { ValidationPipe } from "../pipes/validation.pipe";
 
 @Controller("/users")
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @UsePipes(ValidationPipe)
     @Post()
     create(@Body() userDto: Create_userDto) {
         return this.usersService.createUser(userDto);
