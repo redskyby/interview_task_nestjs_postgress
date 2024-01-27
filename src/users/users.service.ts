425015750f0a5c23666e08c5a4cace1e2147e302
Docenko Pasha <pashadocenko@gmail.com>
2024-01-27 21:20:22 +0300

    Ban was inited.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -43,6 +43,13 @@ export class UsersService {
     }
 
     async ban(dto: Ban_userDto) {
-        return dto;
+        const user = await this.userRepository.findByPk(dto.userId);
+        if(!user){
+            throw new HttpException("Пользователь не найден", HttpStatus.NOT_FOUND);
+        }
+        user.banned = true;
+        user.banReason = dto.banReason;
+        await user.save();
+        return user;
     }
 }
