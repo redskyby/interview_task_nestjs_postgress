9200846cfab10ea6f5b7369c589c43b264717d2d
Docenko Pasha <pashadocenko@gmail.com>
2024-01-27 23:44:03 +0300

    post.module.ts was added.

--- src/users/dto/add_role.dto.ts
+++ src/users/dto/add_role.dto.ts
@@ -1,4 +1,8 @@
+import { IsNumber, IsString } from "class-validator";
+
 export class Add_roleDto {
+    @IsString({ message: "Должно быть строкой" })
     readonly value: string;
+    @IsNumber({}, { message: "Должно быть числом" })
     readonly userId: number;
 }
